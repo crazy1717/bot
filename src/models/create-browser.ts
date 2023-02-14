e9c9fb3ee2d30bc7a077bc514aff8c4ac9f34c49
crazy1717 <125329134+crazy1717@users.noreply.github.com>
2023-02-13 23:55:17 -0300

    Update create-browser.ts

--- src/models/create-browser.ts
+++ src/models/create-browser.ts
@@ -2,17 +2,21 @@ import randomUseragent from "random-useragent";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 import { executablePath } from "puppeteer";
+import chromium from 'chrome-aws-lambda';
 puppeteer.use(StealthPlugin());
 
 const USER_AGENT =
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36";
 
 export async function createPage(url: string) {
-  const browser = await puppeteer.launch({
-    headless: false,
+  const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
     ignoreHTTPSErrors: true,
-    executablePath: executablePath(),
-  });
+  })
+  
   const userAgent = randomUseragent.getRandom();
   const UA = userAgent || USER_AGENT;
   const context = await browser.createIncognitoBrowserContext();
